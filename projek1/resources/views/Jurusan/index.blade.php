5cfecafae198f524e40433ee446a9f489c388c28
M.Farhan Al Ihrami <90372325+1122Arhan@users.noreply.github.com>
2024-05-15 23:29:04 +0700

    Update index.blade.php

--- projek1/resources/views/Jurusan/index.blade.php
+++ projek1/resources/views/Jurusan/index.blade.php
@@ -14,7 +14,7 @@
 @section('content')
     <div class="card">
         <div class="card-header">
-        <h3 class="card-title">TData Jurusan</h3>
+        <h3 class="card-title">Data Jurusan</h3>
 
         <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
